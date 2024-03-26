7d6eb4b98a7151257a30ec215348a515b57f57c4
Jaguruth-N <jagruthjain118@gmail.com>
2024-03-27 00:29:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./layout.scss"
+import Navbar from "./components/navbar/Navbar.jsx"
+
+function App() {
+  return (
+    <div className="layout">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
