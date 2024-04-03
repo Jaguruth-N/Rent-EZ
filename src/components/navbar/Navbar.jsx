2dfdcd4a88126c4b8b5a3ed5e1db8dd983d1c440
KiranDev12 <kirandev.edu@gmail.com>
2024-04-03 22:22:22 +0530

    landing page UI fixes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,49 +3,66 @@ import "./navbar.scss";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const user = true;
-    return (
-        <nav>
-            <div className="left">
-                <a href="/" className="logo">
-                    <img src="/logo.png" alt="" />
-                    <span>Rent EZ</span>
-                </a>
-                <a href="/">Home</a>
-                <a href="/">About</a>
-                <a href="/">Contact</a>
-                <a href="/">Agents</a>
-            </div>
+  const user = true;
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>Property Ease</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
 
-            <div className="right">
-                {user ? (<div className="user">
-                    <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
-                    <span>John Doe</span>
-                    <Link to="/profile" className="profile"><div className="notification">3</div>
-                        <span>Profile</span></Link></div>) : (<>
+      <div className="right">
+        {user ? (
+          <div className="user">
+            <Link to="/profile">
+              <div className="profilePic">
+                <img
+                  src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                  alt=""
+                />
+                <span>John Doe</span>
+              </div>
+            </Link>
 
-                            <a href="/">Sign In</a>
-                            <a href="/" className="register">
-                                Sign Up
-                            </a>
-                        </>
-                )}
-                <div className="menuIcon">
-                    <img src="/menu.png" alt="" onClick={() => setOpen((prev) => !prev)} />
-                </div>
-                <div className={open ? "menu active" : "menu"}>
-                    <a href="/">Home</a>
-                    <a href="/">About</a>
-                    <a href="/">Contact</a>
-                    <a href="/">Agents</a>
-                    <a href="/">Sign In</a>
-                    <a href="/">Sign Up</a>
-                </div>
-            </div>
-        </nav>
-    );
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Chat</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/">Sign In</a>
+            <a href="/" className="register">
+              Sign Up
+            </a>
+          </>
+        )}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign In</a>
+          <a href="/">Sign Up</a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -1,37 +1,49 @@
-import SearchBar from '../../components/searchBar/SearchBar';
-import './homePage.scss'
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import CountUp from "react-countup";
 
 function HomePage() {
-    return (
-        <div className="homePage">
-            <div className="textContainer">
-                <div className="wrapper">
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">
+            Unlock Your Dream Home: Explore Our Listings Today!
+          </h1>
 
-                    <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
-
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam eveniet animi harum voluptate quis facere, magni sapiente eius, itaque natus a similique dolorum vel vitae, ad distinctio obcaecati laboriosam perferendis?</p>
-                    <SearchBar />
-                    <div className="boxes">
-                        <div className="box">
-                            <h1>16+</h1>
-                            <h2>Years of Experience</h2>
-                        </div>
-                        <div className="box">
-                            <h1>200</h1>
-                            <h2>Awards Gained</h2>
-                        </div>
-                        <div className="box">
-                            <h1>1200+</h1>
-                            <h2>Property Ready</h2>
-                        </div>
-                    </div>
-                </div>
+          {/* <p>
+            Discover Your Dream Home Today! Explore our curated collection of
+            exclusive listings tailored to your lifestyle. With easy navigation
+            and comprehensive details, finding your perfect home is just a click
+            away. Start your journey towards homeownership now!
+          </p> */}
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>
+                <CountUp end={16} durations={3} />+
+              </h1>
+              <h2>Years of Experience</h2>
             </div>
-            <div className="imageContainer">
-                <img src="/bg.png" alt="" />
+            <div className="box">
+              <h1>
+                <CountUp end={200} durations={3} />
+              </h1>
+              <h2>Awards Gained</h2>
             </div>
+            <div className="box">
+              <h1>
+                <CountUp end={1200} durations={3} />+
+              </h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
         </div>
-
-    )
+      </div>
+      <div className="imageContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
 }
 export default HomePage;
